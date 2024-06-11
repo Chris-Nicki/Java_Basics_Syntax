b78ae1dbd42962627db73e3ed66467192b50ede9
Chris-Nicki <christopher.nicki@gmail.com>
2024-06-11 08:17:52 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,55 @@
+// Data Types
+let studentName = "Frank";
+let studentAge = 15;
+let studentGrade = 10;
+let studentAttendance = "Present";
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Student Grade:", studentGrade);
+console.log("Student Attendance Status:", studentAttendance);
+
+
+// Operators
+const x = 6;
+const y = 12;
+
+const sum = x + y;
+console.log("Sum:", sum);
+
+const diff = x - y;
+console.log("Difference:", diff);
+
+const product = x * y;
+console.log("Product:", product);
+
+const quot = x / y;
+console.log("Quotient:", quot);
+
+// Update Values
+let myNum1 = 10;
+myNum1 += 10;
+console.log(myNum1)
+
+let myNum2 = 15;
+myNum2 += 15;
+console.log(myNum2)
+
+// Comparison Operators
+let num3 = 15;
+let num4 = 17;
+
+console.log("Is num1 equal to num2?", num3 == num4); 
+console.log("Is num1 greater than num2?", num3 > num4);
+console.log("Is num1 not equal to num2?", num3 != num4); 
+
+// Logical Operators
+
+let myNum5 = 15;
+let myNum6 = 22;
+
+isPositive = (myNum5 > 0) && (myNum6 > 0);
+console.log(isPositive)
+
+isEven = (myNum5 % 2 == 0) || (myNum6 % 2 == 0);
+console.log(isEven)
